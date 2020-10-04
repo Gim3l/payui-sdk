ffb282eae8e294586c5eb4e707ec332759507830
Gimel Dick <gimeldick@gmail.com>
2020-10-04 11:00:38 -0400

    Custom events

--- src/payui-sdk.svelte
+++ src/payui-sdk.svelte
@@ -1,6 +1,25 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
+	import { get_current_component } from 'svelte/internal';
+	const component = get_current_component();
+	const svelteDispatch = createEventDispatcher();
+	const dispatch = (name, detail) => {
+		svelteDispatch(name, detail);
+		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }));
+	};
+
 	export let width = 50;
 	export let height = 50;
+	export let onComplete;
+	const fetchImage = (async () => {
+		const response = await fetch('https://dog.ceo/api/breeds/image/random').then((res) => {
+			dispatch('complete', res);
+
+			return res;
+		});
+
+		return await response.json();
+	})();
 </script>
 
 <style>
@@ -27,11 +46,19 @@
 
 <svelte:options tag="payui-sdk" immutable={true} />
 <main>
-	<img
+	Helo
+	{#await fetchImage}
+		<p>...waiting</p>
+	{:then data}
+		<img src={data.message} alt="Dog image" />
+	{:catch error}
+		<p>An error occurred!</p>
+	{/await}
+	<!-- <img
 		id="barcode"
 		src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&size=100x100"
 		alt=""
 		title="HELLO"
-		width={width}
-		height={height} />
+		{width}
+		{height} /> -->
 </main>
