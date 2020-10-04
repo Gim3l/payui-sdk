705e2e0897ed4089fc5a04059b51a3b4813dc626
Gimel Dick <gimeldick@gmail.com>
2020-10-03 22:31:42 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import PayUISDK from './payui-sdk.svelte';
+
+
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
+
+export default PayUISDK;
---
+++ src/payui-sdk.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let width = 50;
+	export let height = 50;
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<svelte:options tag="payui-sdk" immutable={true} />
+<main>
+	<img
+		id="barcode"
+		src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&size=100x100"
+		alt=""
+		title="HELLO"
+		width={width}
+		height={height} />
+</main>
